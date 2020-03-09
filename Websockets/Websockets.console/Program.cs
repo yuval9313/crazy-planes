b36d14b088ab90c3d52f61f62a11a25242f7e633
Yuval <yuvi9313@gmail.com>
2020-03-09 18:17:22 +0200

    Add websockets client to .NET solution

---
+++ Websockets/Websockets.console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Websockets.core;
+
+namespace Websockets.console
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var websocketClient = new WebsocketClient("ws://localhost:5000");
+            while (true)
+            {
+                Console.WriteLine("Enter a message: ");
+                var message = Console.ReadLine();
+                websocketClient.Send(message);
+                Console.WriteLine(websocketClient.Receive());
+            }
+        }
+    }
+}
---
+++ Websockets/Websockets.core/WebsocketClient.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Websockets.core
+{
+  public class WebsocketClient
+  {
+    private readonly ClientWebSocket _clientSocket;
+
+    public WebsocketClient(string address)
+    {
+      var uri = new Uri(address);
+      _clientSocket = new ClientWebSocket();
+      _clientSocket.ConnectAsync(uri, CancellationToken.None);
+    }
+
+    public async void Send(string message)
+    {
+      ArraySegment<byte> buffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(message));
+      await _clientSocket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+    }
+
+    public string Receive()
+    {
+      return new Lazy<Task<string>>(async () =>
+      {
+        var lengthBuffer = new ArraySegment<byte>(new byte[4]);
+        var _ = await _clientSocket.ReceiveAsync(lengthBuffer, CancellationToken.None);
+        byte[] lenBuf;
+        if (BitConverter.IsLittleEndian)
+        {
+          Console.WriteLine("this");
+          lenBuf = (lengthBuffer.Array ?? throw new NullReferenceException()).Reverse().ToArray();
+        }
+        else
+          lenBuf = lengthBuffer.Array;
+        var len = BitConverter.ToUInt32(lenBuf ?? throw new NullReferenceException(), 0);
+        Console.WriteLine(len);
+        var buffer = new ArraySegment<byte>(new byte[(int) len]);
+        var result = await _clientSocket.ReceiveAsync(buffer, CancellationToken.None);
+        return Encoding.UTF8.GetString(buffer.Array ?? throw new NullReferenceException(), 0, result.Count);
+      }).Value.Result;
+    }
+  }
+}
