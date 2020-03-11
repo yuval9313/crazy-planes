2f01b324c7274fa2e80ae476fe6869013fcd621a
Yuval <yuvi9313@gmail.com>
2020-03-11 02:44:39 +0200

    Add a websocket interface to the project

---
+++ Websockets/Websockets.core/IWebsocketClient.cs
@@ -0,0 +1,8 @@
+﻿namespace Websockets.core
+{
+    public interface IWebsocketClient
+    {
+        string Receive();
+        void Send(string message);
+    }
+}
--- Websockets/Websockets.core/WebsocketClient.cs
+++ Websockets/Websockets.core/WebsocketClient.cs
@@ -7,43 +7,42 @@ using System.Threading.Tasks;
 
 namespace Websockets.core
 {
-  public class WebsocketClient
-  {
-    private readonly ClientWebSocket _clientSocket;
-
-    public WebsocketClient(string address)
+    public class WebsocketClient : IWebsocketClient
     {
-      var uri = new Uri(address);
-      _clientSocket = new ClientWebSocket();
-      _clientSocket.ConnectAsync(uri, CancellationToken.None);
-    }
+        private readonly ClientWebSocket _clientSocket;
 
-    public async void Send(string message)
-    {
-      ArraySegment<byte> buffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(message));
-      await _clientSocket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
-    }
-
-    public string Receive()
-    {
-      return new Lazy<Task<string>>(async () =>
-      {
-        var lengthBuffer = new ArraySegment<byte>(new byte[4]);
-        var _ = await _clientSocket.ReceiveAsync(lengthBuffer, CancellationToken.None);
-        byte[] lenBuf;
-        if (BitConverter.IsLittleEndian)
+        public WebsocketClient(string address)
         {
-          Console.WriteLine("this");
-          lenBuf = (lengthBuffer.Array ?? throw new NullReferenceException()).Reverse().ToArray();
+            var uri = new Uri(address);
+            _clientSocket = new ClientWebSocket();
+            _clientSocket.ConnectAsync(uri, CancellationToken.None);
         }
-        else
-          lenBuf = lengthBuffer.Array;
-        var len = BitConverter.ToUInt32(lenBuf ?? throw new NullReferenceException(), 0);
-        Console.WriteLine(len);
-        var buffer = new ArraySegment<byte>(new byte[(int) len]);
-        var result = await _clientSocket.ReceiveAsync(buffer, CancellationToken.None);
-        return Encoding.UTF8.GetString(buffer.Array ?? throw new NullReferenceException(), 0, result.Count);
-      }).Value.Result;
+
+        public async void Send(string message)
+        {
+            ArraySegment<byte> buffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(message));
+            await _clientSocket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+
+        public string Receive()
+        {
+            return new Lazy<Task<string>>(async () =>
+            {
+                var lengthBuffer = new ArraySegment<byte>(new byte[4]);
+                var _ = await _clientSocket.ReceiveAsync(lengthBuffer, CancellationToken.None);
+                byte[] lenBuf;
+                if (BitConverter.IsLittleEndian)
+                {
+                    lenBuf = (lengthBuffer.Array ?? throw new NullReferenceException()).Reverse().ToArray();
+                }
+                else
+                    lenBuf = lengthBuffer.Array;
+                var len = BitConverter.ToUInt32(lenBuf ?? throw new NullReferenceException(), 0);
+                Console.WriteLine(len);
+                var buffer = new ArraySegment<byte>(new byte[(int)len]);
+                var result = await _clientSocket.ReceiveAsync(buffer, CancellationToken.None);
+                return Encoding.UTF8.GetString(buffer.Array ?? throw new NullReferenceException(), 0, result.Count);
+            }).Value.Result;
+        }
     }
-  }
 }
