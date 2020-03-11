d55216a46a66d1ea156665f5a65dca9c215f9517
Yuval <yuvi9313@gmail.com>
2020-03-11 02:54:16 +0200

    Revert the game console class to be independent main functions

--- CrazyPlanesConsole/game_console.py
+++ CrazyPlanesConsole/game_console.py
@@ -1,31 +1,31 @@
 import time
-
+import json
 from CrazyPlanesCore import GameBoard, StandardStrategy
-from CrazyPlanesConsole.planes.colored_plane import ColoredPlane
+from CrazyPlaneCommon.colored_plane import ColoredPlane
 
 
-class GameConsole:
-    def __init__(self, planes):
-        self.game_board = GameBoard(width, height, planes)
-        self._done = False
-
-    def run(self):
-        while not self._done:
-            state = self.game_board.run()
-            self._done = self.game_board.check_if_done()
-            for plane in state:
-                print(plane.display())
-                time.sleep(0.1)
+def run(game_board):
+    done = False
+    while not done:
+        state = game_board.run()
+        done = game_board.check_if_done()
+        for plane in state:
+            display = json.loads(plane.display())
+            print(f"AirPlane: {display['color']} is in {display['pos']} score: {display['score']}")
+            time.sleep(0.1)
 
 
-if __name__ == '__main__':
+def main():
     print("Welcome to crazy planes")
     width = int(input("Enter the width of the board: "))
     height = int(input("Enter the height of the board: "))
-    game = GameConsole([
+    game_board = GameBoard(width, height, [
         ColoredPlane("red", StandardStrategy(width, height)),
         ColoredPlane("yellow", StandardStrategy(width, height)),
         ColoredPlane("blue", StandardStrategy(width, height))
     ])
+    run(game_board)
 
-    game.run()
+
+if __name__ == '__main__':
+    main()
